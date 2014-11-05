2551b12d056d8fee53d54cb96c057c012db0a247
Krzysiek <krzysztof.wolinski@cohesiva.com>
2014-11-05 23:41:42 +0100

    Improved ShopActivity

--- WiesFight/src/com/wiesfight/activities/ShopActivity.java
+++ WiesFight/src/com/wiesfight/activities/ShopActivity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 public class ShopActivity extends Activity {
 	private User currentUser;
@@ -31,6 +32,8 @@ public class ShopActivity extends Activity {
 		if (currentUser.getUserCoins() >= ATTACK_ITEM_PRICE) {
 			currentUser.setAttackItemCount(currentUser.getAttackItemCount() - ATTACK_ITEM_PRICE);
 			currentUser.addAttackItemCount();
+			
+			refreshCoins();
 		}
 	}
 	
@@ -38,6 +41,8 @@ public class ShopActivity extends Activity {
 		if (currentUser.getUserCoins() >= DEFENSE_ITEM_PRICE) {
 			currentUser.setDefenceItemCount(currentUser.getDefenceItemCount() - DEFENSE_ITEM_PRICE);
 			currentUser.addDefenceItemCount();
+			
+			refreshCoins();
 		}
 	}
 	
@@ -45,6 +50,24 @@ public class ShopActivity extends Activity {
 		if (currentUser.getUserCoins() >= MISC_ITEM_PRICE) {
 			currentUser.setMiscItemCount(currentUser.getMiscItemCount() - MISC_ITEM_PRICE);
 			currentUser.addMiscItemCount();
+			
+			refreshCoins();
 		}
 	}
+	
+	private void refreshCoins() {
+		TextView txt = (TextView) findViewById(R.id.lblCoins);
+    	txt.setText(this.getCoinsString(this.currentUser.getUserCoins()));
+	}
+	
+	private String getCoinsString(int coins) {
+    	String coinString = "monet";
+    	int units = coins % 10;
+    	int dozens = (coins % 100) / 10;
+    	if(coins == 1)
+    		coinString = "moneta";
+    	else if((units == 2 || units == 3 || units == 4) && dozens != 1)
+    		coinString = "monety";
+		return coins + " " + coinString;
+    }
 }
